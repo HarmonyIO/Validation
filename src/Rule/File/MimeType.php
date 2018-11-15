98cff7b2c233642e1712ca8cc8cbcab24e1a8471
Pieter Hordijk <info@pieterhordijk.com>
2018-11-15 18:29:52 +0300

    Added mime type rule
    And removed phpstan checks as currently it is broken software with too many false positives to be worth it

---
+++ src/Rule/File/MimeType.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+
+namespace HarmonyIO\Validation\Rule\File;
+
+use Amp\Promise;
+use Amp\Success;
+use HarmonyIO\Validation\Exception\FileInfo;
+use HarmonyIO\Validation\Rule\Rule;
+use function Amp\call;
+use function Amp\File\exists;
+use function Amp\ParallelFunctions\parallel;
+
+class MimeType implements Rule
+{
+    /** @var string */
+    private $mimeType;
+
+    public function __construct(string $mimeType)
+    {
+        $this->mimeType = $mimeType;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validate($value): Promise
+    {
+        if (!is_string($value)) {
+            return new Success(false);
+        }
+
+        return call(function () use ($value) {
+            if (!yield exists($value)) {
+                return false;
+            }
+
+            return parallel(function () use ($value) {
+                $fileInfo = finfo_open(FILEINFO_MIME_TYPE);
+
+                if ($fileInfo === false) {
+                    throw new FileInfo('Could not open `magic.mime` database.');
+                }
+
+                $mimeType = finfo_file($fileInfo, $value);
+
+                finfo_close($fileInfo);
+
+                return $mimeType === $this->mimeType;
+            })();
+        });
+    }
+}
---
+++ tests/Unit/Rule/File/MimeTypeTest.php
@@ -0,0 +1,81 @@
+<?php declare(strict_types=1);
+
+namespace HarmonyIO\ValidationTest\Unit\Rule\File;
+
+use HarmonyIO\PHPUnitExtension\TestCase;
+use HarmonyIO\Validation\Rule\File\MimeType;
+use HarmonyIO\Validation\Rule\Rule;
+
+class MimeTypeTest extends TestCase
+{
+    public function testRuleImplementsInterface(): void
+    {
+        $this->assertInstanceOf(Rule::class, new MimeType('plain/text'));
+    }
+
+    public function testValidateReturnsFalseWhenPassingAnInteger(): void
+    {
+        $this->assertFalse((new MimeType('plain/text'))->validate(1));
+    }
+
+    public function testValidateReturnsFalseWhenPassingAFloat(): void
+    {
+        $this->assertFalse((new MimeType('plain/text'))->validate(1.1));
+    }
+
+    public function testValidateReturnsFalseWhenPassingABoolean(): void
+    {
+        $this->assertFalse((new MimeType('plain/text'))->validate(true));
+    }
+
+    public function testValidateReturnsFalseWhenPassingAnArray(): void
+    {
+        $this->assertFalse((new MimeType('plain/text'))->validate([]));
+    }
+
+    public function testValidateReturnsFalseWhenPassingAnObject(): void
+    {
+        $this->assertFalse((new MimeType('plain/text'))->validate(new \DateTimeImmutable()));
+    }
+
+    public function testValidateReturnsFalseWhenPassingNull(): void
+    {
+        $this->assertFalse((new MimeType('plain/text'))->validate(null));
+    }
+
+    public function testValidateReturnsFalseWhenPassingAResource(): void
+    {
+        $resource = fopen('php://memory', 'r');
+
+        if ($resource === false) {
+            $this->fail('Could not open the memory stream used for the test');
+
+            return;
+        }
+
+        $this->assertFalse((new MimeType('plain/text'))->validate($resource));
+
+        fclose($resource);
+    }
+
+    public function testValidateReturnsFalseWhenPassingACallable(): void
+    {
+        $this->assertFalse((new MimeType('plain/text'))->validate(static function (): void {
+        }));
+    }
+
+    public function testValidateReturnsFalseWhenFileDoesNotExists(): void
+    {
+        $this->assertFalse((new MimeType('plain/text'))->validate(TEST_DATA_DIR . '/unknown-file.txt'));
+    }
+
+    public function testValidateReturnsTrueWhenFileMatchesMimeType(): void
+    {
+        $this->assertTrue((new MimeType('text/plain'))->validate(TEST_DATA_DIR . '/file-mimetype-test.txt'));
+    }
+
+    public function testValidateReturnsFalseWhenNotMatchingMimeType(): void
+    {
+        $this->assertFalse((new MimeType('application/json'))->validate(TEST_DATA_DIR . '/file-mimetype-test.txt'));
+    }
+}
