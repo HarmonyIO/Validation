d5cc7ccf829d915e3f5ec53d53766cbcf3e0d8be
Pieter Hordijk <info@pieterhordijk.com>
2018-11-15 05:51:22 +0300

    Added text length validators

---
+++ src/Rule/Text/ByteLength.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace HarmonyIO\Validation\Rule\Text;
+
+use Amp\Promise;
+use Amp\Success;
+use HarmonyIO\Validation\Rule\Rule;
+
+class ByteLength implements Rule
+{
+    /** @var int */
+    private $length;
+
+    public function __construct(int $length)
+    {
+        $this->length = $length;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validate($value): Promise
+    {
+        if (!is_string($value)) {
+            return new Success(false);
+        }
+
+        return new Success(strlen($value) === $this->length);
+    }
+}
